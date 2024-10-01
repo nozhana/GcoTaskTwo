c598c0dbc05a0dcb87992c5a724d308314d50bcc
Nozhan Amiri <nozhana@gmail.com>
2024-10-01 13:38:08 +0330

    Initial Commit

---
+++ GcoTaskTwo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GcoTaskTwo
+//
+//  Created by Nozhan Amiri on 10/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GcoTaskTwo/GcoTaskTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GcoTaskTwoApp.swift
+//  GcoTaskTwo
+//
+//  Created by Nozhan Amiri on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GcoTaskTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
